f5e6efcbeaec113eb4ba0dc51084eb1e472f94aa
NejcZdovc <nejc.zdovc@3zsistemi.si>
2019-04-24 17:45:37 +0200

    Generalize twitch event info

--- vendor/bat-native-ledger/src/bat/ledger/internal/media/twitch.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/media/twitch.cc
@@ -77,8 +77,8 @@ std::string MediaTwitch::GetMediaURL(const std::string& media_id) {
 
 // static
 std::string MediaTwitch::GetTwitchStatus(
-    const ledger::TwitchEventInfo& old_event,
-    const ledger::TwitchEventInfo& new_event) {
+    const ledger::MediaEventInfo& old_event,
+    const ledger::MediaEventInfo& new_event) {
   std::string status = "playing";
 
   if (
@@ -111,8 +111,8 @@ std::string MediaTwitch::GetTwitchStatus(
 
 // static
 uint64_t MediaTwitch::GetTwitchDuration(
-    const ledger::TwitchEventInfo& old_event,
-    const ledger::TwitchEventInfo& new_event) {
+    const ledger::MediaEventInfo& old_event,
+    const ledger::MediaEventInfo& new_event) {
   // Remove duplicated events
   if (old_event.event_ == new_event.event_ &&
       old_event.time_ == new_event.time_) {
@@ -303,7 +303,7 @@ void MediaTwitch::ProcessMedia(const std::map<std::string, std::string>& parts,
                                                          TWITCH_MEDIA_TYPE);
   BLOG(ledger_, ledger::LogLevel::LOG_DEBUG) << "Media key: " << media_key;
 
-  ledger::TwitchEventInfo twitch_info;
+  ledger::MediaEventInfo twitch_info;
   std::map<std::string, std::string>::const_iterator iter = parts.find("event");
   if (iter != parts.end()) {
     twitch_info.event_ = iter->second;
@@ -365,7 +365,7 @@ void MediaTwitch::ProcessActivityFromUrl(uint64_t window_id,
 void MediaTwitch::OnMediaPublisherInfo(
     const std::string& media_id,
     const std::string& media_key,
-    const ledger::TwitchEventInfo& twitch_info,
+    const ledger::MediaEventInfo& twitch_info,
     const ledger::VisitData& visit_data,
     const uint64_t window_id,
     const std::string& user_id,
@@ -383,14 +383,14 @@ void MediaTwitch::OnMediaPublisherInfo(
       return;
     }
 
-    ledger::TwitchEventInfo old_event;
-    std::map<std::string, ledger::TwitchEventInfo>::const_iterator iter =
+    ledger::MediaEventInfo old_event;
+    std::map<std::string, ledger::MediaEventInfo>::const_iterator iter =
         twitch_events.find(media_key);
     if (iter != twitch_events.end()) {
       old_event = iter->second;
     }
 
-    ledger::TwitchEventInfo new_event(twitch_info);
+    ledger::MediaEventInfo new_event(twitch_info);
     new_event.status_ = GetTwitchStatus(old_event, new_event);
 
     uint64_t real_duration = GetTwitchDuration(old_event, new_event);
@@ -458,14 +458,14 @@ void MediaTwitch::OnMediaPublisherInfo(
     updated_visit_data.provider = TWITCH_MEDIA_TYPE;
     updated_visit_data.favicon_url = publisher_info->favicon_url;
 
-    ledger::TwitchEventInfo old_event;
-    std::map<std::string, ledger::TwitchEventInfo>::const_iterator iter =
+    ledger::MediaEventInfo old_event;
+    std::map<std::string, ledger::MediaEventInfo>::const_iterator iter =
         twitch_events.find(media_key);
     if (iter != twitch_events.end()) {
       old_event = iter->second;
     }
 
-    ledger::TwitchEventInfo new_event(twitch_info);
+    ledger::MediaEventInfo new_event(twitch_info);
     new_event.status_ = GetTwitchStatus(old_event, new_event);
 
     uint64_t real_duration = GetTwitchDuration(old_event, new_event);
